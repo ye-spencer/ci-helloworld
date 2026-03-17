77caa2a5f14cd0d6640e3b3e39cb992ccfdfda82
Spencer Ye <spencerye425@gmail.com>
2026-03-17 16:12:53 -0400

    Add tests to hit 100% BC

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -45,4 +45,14 @@ public class ArrayUtilsTest {
     public void testOddOrPositiveBothPositivesAndNegatives() {
         assertEquals(3, ArrayUtils.oddOrPos(new int[]{-3, -2, 0, 1, 4}));
     }
+
+    @Test
+    public void testCountOfAllEqual() {
+        assertEquals(2, ArrayUtils.countOf(new int[]{100, 100}, 100));
+    }
+
+    @Test
+    public void testCountOfAllNotEqual() {
+        assertEquals(0, ArrayUtils.countOf(new int[]{100, 100}, 1000));
+    }
 }
